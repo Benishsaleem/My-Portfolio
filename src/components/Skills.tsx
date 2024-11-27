65ebfcac2206a04946fa60dcc93fc33c7b211c07
Benishsaleem <160182625+Benishsaleem@users.noreply.github.com>
2024-11-28 01:12:03 +0500

    made a correction .

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,7 +6,7 @@ const SkillS = () => {
     <div id='skills' className='container pt-32'>
       <div className='grid md:grid-cols-2 gap-20 items-center' data-aos='zoom-in-down'></div>
       <div>
-        <h2 className='text-4xl md:text-5xl'>Technologies I work with</h2>
+        <h2 className='text-4xl md:text-5xl' data-aos='zoom-in-down'>Technologies I work with</h2>
         <p className='text-gray-500 pt-2'>
           I have a strong foundation in Web Development. I am proficient in creating custom websites tailored to unique client requirements. I am well-versed in multiple languages, such as HTML, CSS, Typescript & Jawascript, enabling me to code innovative features & customizations, making each project distinct.
         </p>
